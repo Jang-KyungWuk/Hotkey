572c71f4770cc01adc82ba078d56c55dc76d30c7
Andrewjsy <kj10522002@gmail.com>
2022-12-02 20:23:35 +0900

    1202

--- HotKey_Instagram/Instagram_Crawler_with_DB/Backend_Flask_1201/server_flask.py
+++ HotKey_Instagram/Instagram_Crawler_with_DB/Backend_Flask_1201/server_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, g
+from flask import Flask, jsonify, request, g, render_template
 # customized modules import (사용 라이브러리들 포함)
 # from db import *
 from crawling import *  # db 모듈 포함
@@ -12,16 +12,6 @@ def getdb():
     ### g객체에 할당 코드 (전역변수 할당 코드)
     g.total_acc_info, g.all_blocked = get_accounts()  # all_blocked : 계정이 전부 막혔는지, single_search()에서 활용!! -> 다 막혔으면 서비스가 안됨.
     g.acc_inuse = list()
-    # g.sessions = [
-    #     {
-    #         'session': Session(),
-    #         'inuse': False,
-    #         'last_time': 0,
-    #         'logout': 0,
-    #         'usage': 0,
-    #         'orig_idx': i  # total_acc_info에서의 index
-    #     } for i in range(len(g.total_acc_info))
-    # ]
     print('before_request : DB정보 가져오기 성공')
     ####여기까지 초기 할당
 
@@ -34,9 +24,11 @@ def afterrequest(response):
     print('all_blocked :', g.all_blocked, '\ntotal_acc_info :', g.total_acc_info)
     print('response :', response)
     return response
+
 @app.route('/')
 def home():
     return 'This is the backend server for HOTKEY project__...'
+
 # ---------------------------관리/테스트용 API-------------------------------
 @app.route('/manage/test/keyword_search', methods=['GET'])  # method = GET으로해서
 #http://127.0.0.1:5000/manage/test/keyword_search?keyword=hello와 같이 사용
@@ -44,6 +36,7 @@ def home():
 def keyword_search():
     status, corpus, image = single_search(request.args.get('keyword'))
     return jsonify(status, corpus)
+
 @app.route('/manage/accounts')
 # 현재 전역변수로 저장된 계정 정보를 보여준다.
 def show_accounts():
